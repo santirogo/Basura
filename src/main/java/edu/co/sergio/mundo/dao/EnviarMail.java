551a76663a77d5b0cc463107c30992b542a4a8c1
santirogo <santirogo@gmail.com>
2017-11-01 15:02:06 -0500

    Update EnviarMail.java

--- src/main/java/edu/co/sergio/mundo/dao/EnviarMail.java
+++ src/main/java/edu/co/sergio/mundo/dao/EnviarMail.java
@@ -1,113 +1,31 @@
-package edu.co.sergio.mundo.dao;
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Properties;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-//import util.Conexion;
+import com.sendgrid.*;
+import java.io.IOException;
 
 public class EnviarMail {
-
-    private String cod;
-
-    public EnviarMail() throws URISyntaxException {
-        this.cod = Integer.toString((int) Math.floor(Math.random() * (1000000 - 100000 + 1) + (100000)));
-    }
-
-    public void sendMail(String toAdd) {
-        try {
-            // Propiedades de la conexión
-            Properties props = new Properties();
-            props.setProperty("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port", "587");
-            props.setProperty("mail.smtp.user", "appumartsw@gmail.com");
-            props.setProperty("mail.smtp.auth", "true");
-
-            // Preparamos la sesion
-            Session session = Session.getDefaultInstance(props);
-            
-            System.out.println("Entro Al Metodo De Registro");
-            
-            // Construimos el mensaje
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("appumartsw@gmail.com"));
-            message.addRecipient(
-                    Message.RecipientType.TO,
-                    new InternetAddress(toAdd));
-            message.addHeader("Disposition-Notification-To", "appumartsw@gmail.com");
-            message.setSubject("Verificación de cuenta");
-            message.setText("Tu código: "+this.cod);
-
-            // Lo enviamos.
-            Transport t = session.getTransport("smtp");
-            t.connect("appumartsw@gmail.com", "dondetusicompras");
-            t.sendMessage(message, message.getAllRecipients());
-
-            // Cierre.
-            t.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String getCod() {
-
-        return cod;
-
-    }
+  public EnviarMail(){
     
+  }  
     
-    public void sendCheckOut(String toAdd, ArrayList<String> orden){
-    
-        try {
-            // Propiedades de la conexión
-            Properties props = new Properties();
-            props.setProperty("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port", "587");
-            props.setProperty("mail.smtp.user", "chuidiang@gmail.com");
-            props.setProperty("mail.smtp.auth", "true");
+  public void enviarMail(String toAdd){
+    Email from = new Email("appumartsw@gmail.com");
+    String subject = "Hello World from the SendGrid Java Library!";
+    Email to = new Email("toAdd");
+    Content content = new Content("text/plain", "Hello, Email! <3");
+    Mail mail = new Mail(from, subject, to, content);
 
-            // Preparamos la sesion
-            Session session = Session.getDefaultInstance(props);
-
-            // Construimos el mensaje
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("appumartsw@gmail.com"));
-            message.addRecipient(
-                    Message.RecipientType.TO,
-                    new InternetAddress(toAdd));
-            message.setSubject("Orden");
-            message.setText("Tu orden: ");
-            for (int i = 0; i < orden.size(); i++) {
-                message.setText(orden.get(i));
-            }
-
-            // Lo enviamos.
-            Transport t = session.getTransport("smtp");
-            t.connect("appumartsw@gmail.com", "dondetusicompras");
-            t.sendMessage(message, message.getAllRecipients());
-
-            // Cierre.
-            t.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        
+    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+    Request request = new Request();
+    try {
+      request.method = Method.POST;
+      request.endpoint = "mail/send";
+      request.body = mail.build();
+      Response response = sg.api(request);
+      System.out.println(response.statusCode);
+      System.out.println(response.body);
+      System.out.println(response.headers);
+    } catch (IOException ex) {
+      throw ex;
     }
+  }
 }
-    
+
